fd20063866cedf0b0e065e3430e36d88b01aa533
Gael Sola <gaelsola@hotmail.fr>
2026-03-27 20:39:20 +0100

    added app.R to Rbuildignore

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
+# Run: usethis::use_build_ignore(c("app.R", "rsconnect"))
+# To avoid rsconnect related files to be shipped with the package
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 arenalytics::shiny_run_arenalytics()
