a3fc5edc207e9fdfdaabc44fbea1add77cb70d1a
Gael Sola <gaelsola@hotmail.fr>
2026-07-03 23:23:21 +0200

    updated demo file

--- R/mod_about_server.R
+++ R/mod_about_server.R
@@ -8,10 +8,9 @@ mod_about_server <- function(id, rv) {
     ns <- session$ns
 
     output$dl_example <- downloadHandler(
-      filename = function(){"OLAP_Shiny_demo.zip"},
+      filename = function(){"MAU_Shiny_demo.zip"},
       content  = function(file){file.copy(system.file("extdata/MAU_Shiny_(testi_4_chain).zip", package = "arenalytics"), file)}
     )
-
 
   }) ## END module server function
 
