69feac2f3a1c760d63cfa11f1fcca7569a7e0c4d
Gael Sola <gaelsola@hotmail.fr>
2026-07-05 20:28:49 +0200

    cosmetic/data check

--- tests/check-data.R
+++ tests/check-data.R
@@ -1,44 +1,44 @@
-
-library(tidyverse)
-library(arenalytics)
-
-zipfile <- system.file("extdata/MAU_Shiny_(testi_4_chain).zip", package = "arenalytics")
-if (nzchar(zipfile) && file.exists(zipfile)) {
-  zipdata <- fct_readzip(.path = zipfile)
-  summary(zipdata$OLAP_tree$tree_biomass_ag)
-}
-
-names(zipdata$data)
-
-chain <- zipdata$data$chain_summary
-chain_var <- as_tibble(chain$resultVariables)
-
-table(chain_var$active)
-
-n_var <- chain_var |>
-  filter(active) |>
-  group_by(entity, areaBased) |>
-  summarise(n_var = n(), .groups = "drop") |>
-  mutate(areaBased = if_else(areaBased, "areaBased", "notAreaBased")) |>
-  pivot_wider(names_from = areaBased, values_from = n_var) |>
-  mutate(
-    areaBased = if_else(is.na(areaBased), 0, areaBased),
-    notAreaBased = if_else(is.na(notAreaBased), 0, notAreaBased)
-  )
-
-schema <- as_tibble(zipdata$SchemaSummary)
-
-dims <- as_tibble(zipdata$ReportDimensions)
-
-tree <- as_tibble(zipdata$data$MAU_tree)
-tree
-
-bamboo <- as_tibble(zipdata$data$MAU_bamboo)
-names(tree)
-summary(tree)
-
-tree_bu <- tree |> filter(mau_baseunit_total)
-
-taxo <- zipdata$data$taxonomies
-
-taxo
+#
+# library(tidyverse)
+# library(arenalytics)
+#
+# zipfile <- system.file("extdata/MAU_Shiny_(testi_4_chain).zip", package = "arenalytics")
+# if (nzchar(zipfile) && file.exists(zipfile)) {
+#   zipdata <- fct_readzip(.path = zipfile)
+#   summary(zipdata$OLAP_tree$tree_biomass_ag)
+# }
+#
+# names(zipdata$data)
+#
+# chain <- zipdata$data$chain_summary
+# chain_var <- as_tibble(chain$resultVariables)
+#
+# table(chain_var$active)
+#
+# n_var <- chain_var |>
+#   filter(active) |>
+#   group_by(entity, areaBased) |>
+#   summarise(n_var = n(), .groups = "drop") |>
+#   mutate(areaBased = if_else(areaBased, "areaBased", "notAreaBased")) |>
+#   pivot_wider(names_from = areaBased, values_from = n_var) |>
+#   mutate(
+#     areaBased = if_else(is.na(areaBased), 0, areaBased),
+#     notAreaBased = if_else(is.na(notAreaBased), 0, notAreaBased)
+#   )
+#
+# schema <- as_tibble(zipdata$SchemaSummary)
+#
+# dims <- as_tibble(zipdata$ReportDimensions)
+#
+# tree <- as_tibble(zipdata$data$MAU_tree)
+# tree
+#
+# bamboo <- as_tibble(zipdata$data$MAU_bamboo)
+# names(tree)
+# summary(tree)
+#
+# tree_bu <- tree |> filter(mau_baseunit_total)
+#
+# taxo <- zipdata$data$taxonomies
+#
+# taxo
