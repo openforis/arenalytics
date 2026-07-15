b7b236ec3db21e8a85aa346f4777c57fe4b22f7c
Gael Sola <gaelsola@hotmail.fr>
2026-07-15 13:24:12 +0200

    renamed demo file

--- R/mod_about_server.R
+++ R/mod_about_server.R
@@ -9,7 +9,7 @@ mod_about_server <- function(id, rv) {
 
     output$dl_example <- downloadHandler(
       filename = function(){"MAU_Shiny_demo.zip"},
-      content  = function(file){file.copy(system.file("extdata/MAU_Shiny_(testi_4_chain).zip", package = "arenalytics"), file)}
+      content  = function(file){file.copy(system.file("extdata/MAU_Shiny_demo.zip", package = "arenalytics"), file)}
     )
 
   }) ## END module server function
